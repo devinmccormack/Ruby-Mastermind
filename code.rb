9f3b9b8de34c0c193e13bf2cd2c99f1d35d52f8b
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-08-27 13:07:41 -0400

    Add functionality for code object with colors reduced to just one character for convenience

---
+++ code.rb
@@ -0,0 +1,42 @@
+class Code
+  attr_reader :code
+
+  COLORS = {
+    'R' => 'Red',
+    'B' => 'Blue',
+    'G' => 'Green',
+    'Y' => 'Yellow',
+    'O' => 'Orange',
+    'P' => 'Purple'
+  }
+
+  def initialize
+    @code = []
+  end
+
+  def set_code(code)
+    @code = code.upcase.chars
+  end
+
+  def generate_random_code
+    @code = Array.new(4) { COLORS.keys.sample }
+  end
+
+  def generate_feedback(guess)
+    feedback = []
+    guess.upcase.chars.each_with_index do |char, index|
+      if char == @code[index]
+        feedback << 'correct'
+      elsif @code.include?(char)
+        feedback << 'wrong position'
+      else
+        feedback << 'incorrect'
+      end
+    end
+    feedback
+  end
+
+  def reveal_code
+    @code.join
+  end
+end
