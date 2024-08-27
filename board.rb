838736242cd8834f33396f4c19a8d2614063bb46
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-08-27 13:00:14 -0400

    Implement basic funtionality to the board class

---
+++ board.rb
@@ -0,0 +1,22 @@
+class Board
+  MAX_GUESSES = 12
+
+  def initialize
+    @guesses = []
+  end
+
+  def display
+    puts "Current Board:"
+    @guesses.each_with_index do |(guess, feedback), index|
+      puts "Guess ##{index + 1}: #{guess.join(' ')} | Feedback: #{feedback.join(' ')}"
+    end
+  end
+
+  def store_guess(guess, feedback)
+    @guesses << [guess, feedback]
+  end
+
+  def guess_limit_reached?
+    @guesses.length >= MAX_GUESSES
+  end
+end
