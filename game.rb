2567d05abb21712043b064e710f1794166a4bd56
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-08-27 11:17:39 -0400

    Set up object main to include choosing a role

---
+++ game.rb
@@ -0,0 +1,66 @@
+require './player.rb'
+require './board.rb'
+require './code.rb'
+require './feedback.rb'
+
+class Game
+  def initialize(role)
+    @board = Board.new
+    @code = Code.new
+    @codemaker = nil
+    @codebreaker = nil
+    setup_players(role)
+  end
+
+  def play
+    setup_code
+
+    until game_over?
+      @board.display
+      guess = @codebreaker.make_guess
+      feedback = @code.generate_feedback(guess)
+      @board.store_guess(guess, feedback)
+      display_feedback(feedback)
+    end
+
+    end_game_message
+  end
+
+  private
+
+  def setup_players(role)
+    if role == "codemaker"
+      @codemaker = Player.new("You", :human)
+      @codebreaker = Player.new("Computer", :computer)
+    else
+      @codemaker = Player.new("Computer", :computer)
+      @codebreaker = Player.new("You", :human)
+    end
+  end
+
+  def setup_code
+    if @codemaker.is_human?
+      code = @codemaker.create_code
+      @code.set_code(code)
+    else
+      @code.generate_random_code
+    end
+  end
+
+  def game_over?
+    @codebreaker.has_won? || @board.guess_limit_reached?
+  end
+
+  def display_feedback(feedback)
+    puts "Feedback: #{feedback}"
+  end
+
+  def end_game_message
+    if @codebreaker.has_won?
+      puts "#{@codebreaker.name} has cracked the code! Congratulations!"
+    else
+      puts "#{@codemaker.name}'s code remained unbroken!"
+      puts "The code was: #{@code.reveal_code}"
+    end
+  end
+end
---
+++ main.rb
@@ -0,0 +1,36 @@
+require './player.rb'
+require './board.rb'
+require './game.rb'
+require './code.rb'
+require './feedback.rb'
+
+class Main
+  def self.start
+    puts "Welcome to Mastermind!"
+    
+    loop do
+      puts "Do you want to be the codemaker or the codebreaker? (codemaker/codebreaker)"
+      role = gets.chomp.downcase
+      if (role == 'codemaker' || role == 'codebreaker')
+        break
+      else 
+        puts "Please choose a valid role to play the game."
+      end
+    end
+
+    # Start a new game with player's role
+    game = Game.new(role)
+    game.play
+
+    # Option to restart the game
+    puts "Do you want to play again? (yes/no)"
+    answer = gets.chomp.downcase
+    if answer == "yes"
+      start # Recursively call start to restart the game
+    else
+      puts "Thanks for playing!"
+    end
+  end
+end
+
+Main.start
